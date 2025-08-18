deb61f6d7279ce647ddc8b1b76faf9e18e37e0aa
Mubashir-cripto <mubashirkhawer555@gmail.com>
2025-08-18 21:39:16 +0500

    Vedio 055 Completed

---
+++ Vedio 055/Quiz_and_Chapter1.js
@@ -0,0 +1,50 @@
+// Quick Quiz: Write a Javascipt program to store Phone number and márks of a student using objets
+
+// START OF QUIZ
+
+// let m_obj = {
+// "name" :"dawer tariq",
+// "Phone number": "121-399-120-9922",
+// marks: 880
+// }
+// console.log(m_obj);
+// console.log(typeof m_obj);
+
+// END OF QUIZ
+
+// Chapter 1- Practice Set
+
+// START OF CHAPTER 1 PRACTICE SET :-
+
+// 1: Create a variable of type sting and try to add a numter to it
+// let str = "The output is: " + 19
+// console.log(str)
+
+// 2: Use typeof operator to find the datatype of the string  in last question
+// console.log(typeof str)
+
+// 3: Create a const object in javascript. Can you change it to hold a number later?
+// const obj = {
+//     Name : "MOBI"
+// }
+
+// obj = 12;
+// console.log(obj)       //Wrong
+
+// obj.Name = "1000 RS";
+// console.log(obj)       //Correct
+
+// 4. Try to add a new key to the const object in Problem 3.Were you able to do it?
+// obj.newKey = 1233;
+// console.log(obj)     //Key is property and "object_name" is obj
+
+// 5. Write a JS program to create a word-meaning dictionary of 5 words.
+let dict = {
+    Mubashir : "Hapiness bringer",
+    dawer : "Justice bringer",
+    jawad : "Big Hearted",
+    "Abdus Salam" : "Servant of God",
+    Naheed : "Beautifull"
+}
+
+console.log(dict)
