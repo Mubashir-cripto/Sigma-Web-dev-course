a514f24c21da01852202431e83236e4f796fe7bf
Mubashir-cripto <mubashirkhawer555@gmail.com>
2025-08-19 18:35:22 +0500

    Vedio 056 Completed

---
+++ Vedio 056/PracticeSet.js
@@ -0,0 +1,73 @@
+
+// Chapter 2- Practice Set
+/*
+
+Use logial operators to find whether the age of a perion lies betucen 10 and 20?
+
+let age = 20;
+
+if(age>=10 && age<=20){
+    console.log("Age is between 10 and 20")
+}
+
+else{
+    console.log("age is not in range! kindly grow up")
+}
+
+*/
+
+/*
+Demonstrate the use of swith case stakments
+in Javascript
+let age = 12;
+
+if(age>=18){
+    console.log("You are a Pakistani Adult")
+}
+
+else if(age>=13 && age<=17){
+    console.log("You are an young man")
+}
+
+else{
+    console.log("Age not meet the requirements for the registration")
+console.log("TRY NEXT TIME")
+}
+*/
+
+/*
+Write a Java Scrift program to find whether a
+numtber is Divisible by 2 and 3
+
+let num = 10;
+
+if(num%2===0 && num%3===0){
+    console.log("The number is divisible by 2 and 3")
+}
+
+else{
+    console.log("The number is not divisible by 2 and 3")
+}
+*/
+
+/*
+Write a JavaScript program to find whether a
+number is Divislble By either 2 or 3
+
+let num = 10;
+
+if(num%2===0 || num%3===0){
+    console.log("The number is divisible by either 2 or 3")
+}
+
+else{
+    console.log("The number is not divisible by 2 and 3")
+}
+*/
+
+/*
+Print "You can Drive" or "You cannot Drive" based on agе age being greater than 18 using ternary operator
+
+let age = 19;
+(age>18)? console.log("You can Drive") : console.log("You cannot drive")
+*/
