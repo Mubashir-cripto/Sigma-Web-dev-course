1030e42a45ee6646da8d04b5665dc345b4e62f33
Mubashir-cripto <mubashirkhawer555@gmail.com>
2025-08-23 13:12:52 +0500

    Vedio 060 Completed(strings)

---
+++ Vedio 060/PracticeSet.js
@@ -0,0 +1,42 @@
+// Chapter 4- Practice Set
+
+/*
+What will the following print in JavaScript?
+console.log(" har\"".length)
+
+// : 5 <----> It will not print har"  
+*/
+
+/*
+Explore the includes, startsWith & endsWith functions of a sting
+let a = "Rohan Singh"
+
+console.log(a.includes("Rohan"))
+
+console.log(a.startsWith("Rohan"))
+console.log(a.startsWith("Singh", 6))
+
+console.log(a.endsWith("gh"))
+*/
+
+/*
+Write a program to convert a given string to lowercase.
+
+let str_name = "SAMI"
+console.log(str_name.toLowerCase())
+*/
+
+/*
+Extract the amount out of this string
+"Please give Rs 1000"
+
+let amount = "Please give Rs 1000"
+console.log(amount.slice(15))
+*/
+
+/*
+Try to Change 4" charates of a girven stingWere you eble to do it
+
+let a = "Mobi"
+console.log(a.replace("i","e"))
+*/
---
+++ Vedio 060/index.js
@@ -0,0 +1,37 @@
+console.log("This is strings tutorial")
+
+let a = "Mubashir";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+// console.log(a[8]);
+
+console.log(a.length)
+
+let real_name = "Mubashir"
+let friend = "Sami-ullah"
+console.log("His name is "+ real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let b = "ShivamSh"
+console.log(b.toUpperCase()) 
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5))
+console.log(b.slice(1))
+
+console.log(b.replace("Sh", "78"))
+
+console.log(b.concat(a, "Aishwariya" , "Rahul" , "Priya"))
+
+let name = "    Jawad"
+let newName = name.trim()
+console.log(newName)
+
+console.log(b)
+// Strings are immutable(Not Changable)
