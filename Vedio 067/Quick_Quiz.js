a1f5bdc83412132b9d931315c893303e0022dd44
Mubashir-cripto <mubashirkhawer555@gmail.com>
2025-08-31 19:57:13 +0500

    Delete Vedio 067 directory

--- Vedio 067/Quick_Quiz.js
+++
@@ -1,8 +0,0 @@
-alert (document.documentElement.parentNode);
-alert ( document.documentElement.parentElement);
-
-// // Quick-Quiz
-//  Print typeof document and typeof window in the console & see what it prints.
-console.log(typeof(document))
-console.log(typeof(window))
-
