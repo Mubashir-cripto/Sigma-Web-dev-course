d0374650bb626a946a6b38603831cf9546ba04e3
Mubashir-cripto <mubashirkhawer555@gmail.com>
2025-08-31 15:20:35 +0500

    Vedio 067 Completed
    About DOM and a quick_quiz

---
+++ Vedio 067/Quick_Quiz.js
@@ -0,0 +1,8 @@
+alert (document.documentElement.parentNode);
+alert ( document.documentElement.parentElement);
+
+// // Quick-Quiz
+//  Print typeof document and typeof window in the console & see what it prints.
+console.log(typeof(document))
+console.log(typeof(window))
+
